ddb4716d4fd5c6c71db301c279e3c6deb73ac046
DAmniTza <139076154+DAmniTza@users.noreply.github.com>
2024-03-22 05:45:22 +0700

    Update and rename omni_1818.mk to twrp_1818.mk

--- twrp_1818.mk
+++ twrp_1818.mk
@@ -5,18 +5,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from 1818 device
 $(call inherit-product, device/vivo/1818/device.mk)
 
 PRODUCT_DEVICE := 1818
-PRODUCT_NAME := omni_1818
+PRODUCT_NAME := twrp_1818
 PRODUCT_BRAND := vivo
 PRODUCT_MODEL := vivo 1818
 PRODUCT_MANUFACTURER := vivo
