89de8254b892e0b47fabdf52089c1ad35833db92
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-03-21 14:15:37 +0100

    1818: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: vivo Model: vivo 1818 Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_1818.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_1818-user \
+    omni_1818-userdebug \
+    omni_1818-eng
